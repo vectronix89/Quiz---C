5015f386e5a1e2e29382ac21455e93a1d64dede2
ahmad ilyas <vectronix89@gmail.com>
2018-02-01 13:48:01 +0700

    Initial Commit

---
+++ RakutenQuizC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RakutenQuizC
+//
+//  Created by ahmad ilyas on 01/02/18.
+//  Copyright © 2018 ahmad ilyas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
